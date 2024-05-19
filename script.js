930d1c87b2e9494d2dc4c3a973bbc9f1c91537f2
Shubz19 <163107930+Shubz19@users.noreply.github.com>
2024-05-19 19:27:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// var typed = new Typed((".skills"),{
+// 	strings : ["Frontend Developer","Youtuber","Ui Designer"],
+// 	typeSpeed : 100,
+// 	backSpeed : 100,
+// 	backdelay : 100,
+// 	loop : true
+// });	
